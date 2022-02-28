cdff420ffa451c5f5333a2f2c05cbd8b70aa8d84
Hünkar Genç Yıldız <hunkar.genc11@hacettepe.edu.tr>
2022-02-28 21:35:22 +0300

    live 7

--- vetproject/settings.py
+++ vetproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$v!j(3w5o6#@!=tid85caqet77jl_fv3ye4x$)hai^m5!i=bx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vetapp-live.herokuapp.com','127.0.0.1']
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
